67554355f69249f7facfe4e1bf9c08d979ab67f1
RegisSalesRA <regismsales@gmail.com>
2024-08-11 23:06:14 -0300

    flask update

--- server/model.py
+++ server/model.py
@@ -1,15 +1,31 @@
 from config import db
 
-class Contact(db.Model):
+
+class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80),unique=False, nullable=False)
-    last_name = db.Column(db.String(80),unique=False, nullable=False)
-    email = db.Column(db.String(120),unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
 
     def to_json(self):
-         return {
-              "id": self.id,
-              "firstName": self.first_name,
-              "lastName": self.last_name,
-              "email": self.email
-         }
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
+    
+    group = db.relationship('Group', backref=db.backref('contacts', lazy=True))
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "email": self.email,
+            "group": self.group.to_json()
+        }
